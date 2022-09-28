99f3d0dcfe6baa73557bdee0a8e61939825bc21a
pau-regalado <alu0101330174@ull.edu.es>
2022-09-28 18:45:35 +0000

    Añadida descripcion

--- pr1-SimbolosAlfabetosYLenguajes/src/cadena.cc
+++ pr1-SimbolosAlfabetosYLenguajes/src/cadena.cc
@@ -1,3 +1,15 @@
+// Universidad de La Laguna
+// Escuela Superior de Ingeniería y Tecnología
+// Grado en Ingeniería Informática
+// Asignatura: Computabilidad y Algoritmia
+// Curso: 2º
+// Practica 1: Símbolos, alfabetos y cadenas
+// Autor:Paula Regalado de León
+// Correo: alu0101330174@ull.es
+// Fecha: 28/09/2022
+// Archivo :
+// Contiene
+
 #include "../include/cadena.h"
 
 Cadena::Cadena() {
